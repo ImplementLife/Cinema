cd2f08cd2b90e536c3a8702b8465509f2dc27ccf
xomastyle- <homenich101@gmail.com>
2022-10-07 15:23:19 +0300

    default redux structure

--- apps/cinema-web/client/src/index.tsx
+++ apps/cinema-web/client/src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import { setupStore } from './redux-rtk-query/store/';
 import reportWebVitals from './reportWebVitals';
+
+const store = setupStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
