e8cefc6cf7b89db24d1691f1b13786dac2734895
xomastyle- <homenich101@gmail.com>
2022-10-10 15:58:56 +0300

    git jokes

--- apps/cinema-web/client/src/router/router.ts
+++ apps/cinema-web/client/src/router/router.ts
@@ -1,4 +1,4 @@
-import MainPage from "../pages/MainPage";
+import MainPage from "../pages/Public/MainPage";
 import { RouterTypes } from '../models/RouterTypes';
 
 export const publicRoutes: RouterTypes[] = [
